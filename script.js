596feb2eeeb803c18d7ecdba6c9c2b9134c5a211
mnursarp <mnrsarp@gmail.com>
2023-09-26 11:49:38 +0300

    movie list kaydırma işlemi tamamlandı

---
+++ script.js
@@ -0,0 +1,17 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll("img").length;
+  arrow.addEventListener("click", function () {
+    clickCounter++;
+    if (imageItem - (4 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 280
+      }px)`; /* backtick için alt + 96*/
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+    }
+  });
+});
